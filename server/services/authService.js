e4f776460718c085e957e64e23f5ad8876baca25
savki <savkin@altacod.ru>
2024-11-14 21:22:33 +0300

    fix eslint

--- server/services/authService.js
+++ server/services/authService.js
@@ -1,6 +1,6 @@
 // src/services/authService.js
 
-const allowedUsers = ['user1', 'user2', 'user3']; // Список разрешенных пользователей
+const allowedUsers = ['428540866', '481451826', '1833578140']; // Список разрешенных пользователей
 
 export const isUserAllowed = (username) => {
     return allowedUsers.includes(username);
